6ccb3353c6a7b00440bab0b05b9309b888de2944
Wanjohi <kazibin66@gmail.com>
2025-09-25 10:33:47 +0300

    Learnt about the Scanner class used to read user inputs

---
+++ src/Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.println("You have input " + number);
+    }
+}
